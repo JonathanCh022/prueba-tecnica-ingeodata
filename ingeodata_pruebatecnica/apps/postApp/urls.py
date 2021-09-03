2ade8b8122635ff68f7ee16be476f995c6355f00
JonathanCh022 <chacon_022@hotmail.com>
2021-09-02 19:12:46 -0500

    add new url path

--- ingeodata_pruebatecnica/apps/postApp/urls.py
+++ ingeodata_pruebatecnica/apps/postApp/urls.py
@@ -3,4 +3,5 @@ from ingeodata_pruebatecnica.apps.postApp.views import MyFormView
 
 urlpatterns = [
     path('save', MyFormView.as_view(), name='myformAPI'),
+    path('show', MyFormView.as_view(), name='myformAPI'),
 ]
